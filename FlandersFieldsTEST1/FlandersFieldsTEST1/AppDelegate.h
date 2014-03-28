31e6b126d7acfa63a7eb8d2fa93417aa3432a096
TomVanleenhove <vanleenhove2@gmail.com>
2014-03-28 01:15:03 +0100

    nog zottere code

--- FlandersFieldsTEST1/FlandersFieldsTEST1/AppDelegate.h
+++ FlandersFieldsTEST1/FlandersFieldsTEST1/AppDelegate.h
@@ -3,7 +3,7 @@
 //  FlandersFieldsTEST1
 //
 //  Created by Renaat Stuijk on 25/03/14.
-//  Copyright (c) 2014 Renaat Stuijk. All rights reserved.
+//  Copyright (c) 2014 Renaat Stuijk en Tom Vanleenhove. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
